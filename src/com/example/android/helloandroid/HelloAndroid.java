898904c1e3993d19bfe162de849f3ffa1b1de557
thornbird <thornbird.blue@gmail.com>
2022-01-16 15:38:59 +0800

    ci origin code.

---
+++ src/com/example/android/helloandroid/HelloAndroid.java
@@ -0,0 +1,14 @@
+package com.example.android.helloandroid;
+
+//import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle;
+import android.app.Activity;
+
+public class HelloAndroid extends Activity {
+	    protected void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.hello_activity);
+	    }
+}
+
+
